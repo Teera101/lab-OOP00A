3a0670279100f713898d9c2b4e67589b09157a9a
N <119285336+Teera101@users.noreply.github.com>
2022-11-28 15:38:20 +0700

    OOP00A update1

---
+++ lab-OOP00A/lab-OOP00A/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// name
+Console.WriteLine("Teera rewtawee");
+// id
+Console.WriteLine("653450288-5");
+// email
+Console.WriteLine("teera.r@kkumail.com");
